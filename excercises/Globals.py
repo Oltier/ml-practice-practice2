18aa90fd337381d38da8218d89be90480a7327bf
Zoltán Tudlik <zoltan.tudlik@gmail.com>
2018-09-18 20:24:13 +0300

    add globals to just load for everything

---
+++ excercises/Globals.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+def normalize(x, min, max):
+    return (x - min) / (max - min)
+
+
+def normalize_features(features):
+    return list(map(lambda i: normalize(i, np.min(features), np.max(features)), features))
+
+global df_bc, df_eth, bitcoin, ethereum, axis, best_alpha
+# read in historic Bitcoin and Ethereum statistics data from the files "BTC-USD.csv" and "ETH-USD.csv"
+
+df_bc = pd.read_csv("BTC-USD.csv", parse_dates=['Date'])
+df_eth = pd.read_csv("ETH-USD.csv", parse_dates=['Date'])
+
+# read the dates for the individual records from column "Date"
+bitcoin_date = df_bc.Date.values
+ethereum_date = df_eth.Date.values
+
+xs = df_bc.Close.values
+ys = df_eth.Close.values
+
+bitcoin = normalize_features(xs)
+ethereum = normalize_features(ys)
+
+global x, y
+x = bitcoin
+y = ethereum
+
+# plt.show()
--- excercises/LinearRegression.py
+++ excercises/LinearRegression.py
@@ -1,9 +1,13 @@
+from excercises.Globals import *
+
+
 def featureMatrix(x):
     # Generate x_i = (x_i, 1) feature matrix
     # x_i = ...
     # YOUR CODE HERE
     raise NotImplementedError()
     return x_i
+
 
 def fit(x, y):
     ### STUDENT TASK ###
@@ -13,6 +17,7 @@ def fit(x, y):
     # YOUR CODE HERE
     raise NotImplementedError()
     return w_opt
+
 
 # Predict new y data
 # return numpy.ndarray:
@@ -28,6 +33,7 @@ def predict(X, w_opt):
     raise NotImplementedError()
     return y_pred
 
+
 # Calculate empirical error of the prediction
 # return float
 def empirical_risk(X, Y, w_opt):
@@ -39,6 +45,7 @@ def empirical_risk(X, Y, w_opt):
     # YOUR CODE HERE
     raise NotImplementedError()
     return empirical_error
+
 
 def linearRegression(x, y):
     ### STUDENT TASK ###
@@ -52,33 +59,33 @@ def linearRegression(x, y):
     raise NotImplementedError()
     return w_opt, empirical_error
 
+
 def labelVector(y):
     # Reshape y to ensure correct behavior when doing matrix operations
-    return np.reshape(y,(len(y),1))
+    return np.reshape(y, (len(y), 1))
+
 
 def draw_plot(x, y, title=''):
     w_opt, empirical_error = linearRegression(x, y)
-    x_pred = np.linspace(0,1.1,100)
+    x_pred = np.linspace(0, 1.1, 100)
     y_pred = predict(featureMatrix(x_pred), w_opt)
     # Plot data points and linear regression fitting line
     plt.figure(figsize=(8, 8))
     plt.scatter(x, y)
-    plt.plot(x_pred,y_pred,'r', label=("Empirical = %.4f" % empirical_error))
+    plt.plot(x_pred, y_pred, 'r', label=("Empirical = %.4f" % empirical_error))
     plt.title(title)
     plt.xlabel('Bitcoin')
     plt.ylabel('Ethereum')
     plt.legend()
+    global axis
     axis = plt.gca()
 
 
 ######### Linear regression model for x and y data #########
 draw_plot(x, y, r'$\bf{Figure\ 4.}$ Normalized cryptocurrency prices')
 
-
-
-
 w_opt, empirical_error = linearRegression(x, y)
 assert empirical_error < 0.015
-w_opt, empirical_error = linearRegression([0,1,2,3], [0,1,2,3])
+w_opt, empirical_error = linearRegression([0, 1, 2, 3], [0, 1, 2, 3])
 # Because of computational rounding errors, empirical error is almost never exactly 0
 assert empirical_error < 1e-30
